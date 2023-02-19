cea7456fb472e1e191980167900e1b78d108ed19
SpaceTacossss <upbeaterol@gmail.com>
2023-02-19 09:19:21 -0500

    gytgtygt7

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
-import { ThemeProvider } from "react-native-rapi-ui";
+import { ThemeProvider} from "react-native-rapi-ui";
 export default function App() {
   return (
     <ThemeProvider>
