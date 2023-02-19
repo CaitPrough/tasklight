0fc478bb3d194e8f3637e10fe15906c4605de8d5
SpaceTacossss <upbeaterol@gmail.com>
2023-02-19 09:31:07 -0500

    date parsing, some error prevention

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Task from "./Task";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { ThemeProvider} from "react-native-rapi-ui";
 export default function App() {
--- Task.js
+++ Task.js
@@ -3,13 +3,23 @@ import AppNavigator from "./src/navigation/AppNavigator";
 import { ThemeProvider} from "react-native-rapi-ui";
 
 let taskText = ["I am an example task. Add more below."]
-let taskDeadline = ["2/19/23"]
+let taskDeadline = ["02/19/23"]
 
 export function addTask(string, deadline) {
-    taskText.push(string)
-    taskDeadline.push(deadline)
+    if (deadline.length == 8) {
+        taskText.push(string)
+        taskDeadline.push(deadline)
+    }
+    // return error if false
 }
 
 export function getTask(index) {
     return [taskText[index], taskDeadline[index]]
+}
+
+export function parseDate (date) {
+    let month = date.substring(0, 1)
+    let day = date.substring(3, 4)
+    let year = date.substring(6, 7)
+    return [month, day, year]
 }
