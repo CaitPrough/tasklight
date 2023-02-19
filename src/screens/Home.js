c5570e1b6b913c1380298b50124c331410f6b632
purpleapplebird <annewu109@gmail.com>
2023-02-19 16:04:14 -0500

    checkboxes working, kind of

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,23 +9,51 @@ import {
   SectionContent,
   useTheme,
   TextInput,
+  CheckBox,
 } from "react-native-rapi-ui";
 import TaskInput from "../components/utils/NewTask";
-
+//import Forms from "../components/testincomponent";
 
 
 export default function ({ navigation }) {
     const { isDarkmode, setTheme } = useTheme();
-    //To do list//
-    //Add checklists here
+    const [checkBox, setCheckbox] = React.useState(false)
+    //To do list
     return (
     <Layout>
         <ScrollView>
             <View style={{alignItems:"center", marginVertical:20}}>
                 <Text size="h1" fontWeight="bold" style={{color: isDarkmode ? 'white' : 'black'}}>Tasklight <FontAwesomeIcon size={35} name="lightbulb-o"></FontAwesomeIcon> </Text>
             </View>
+            <View style={{flexDirection: 'row', alignItems:"center", marginVertical:10, marginHorizontal:20}}>
+            <CheckBox value={checkBox} onValueChange={(val) => setCheckbox(val)} />
+                <Section style={{marginHorizontal:20, alignItems:"center"}}>
+                    <SectionContent>
+                        <Text>Welcome</Text>
+                    </SectionContent>
+                </Section>
+            </View>
+            <View style={{flexDirection: 'row', alignItems:"center", marginVertical:10, marginHorizontal:20}}>
+            <CheckBox value={checkBox} onValueChange={(val) => setCheckbox(val)} />
+                <Section style={{marginHorizontal:20, alignItems:"center"}}>
+                    <SectionContent>
+                        <Text>Welcome</Text>
+                    </SectionContent>
+                </Section>
+            </View>
+            <View style={{flexDirection: 'row', alignItems:"center", marginVertical:10, marginHorizontal:20}}>
+            <CheckBox value={checkBox} onValueChange={(val) => setCheckbox(val)} />
+                <Section style={{marginHorizontal:20, alignItems:"center"}}>
+                    <SectionContent>
+                        <Text>Welcome</Text>
+                    </SectionContent>
+                </Section>
+            </View>
             
         </ScrollView>
+                 
+        
+   
     </Layout>
     )
 }
