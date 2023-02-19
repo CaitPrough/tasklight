049a1231da54832702e8829b5a51a62b9f7eba49
SpaceTacossss <upbeaterol@gmail.com>
2023-02-19 09:52:14 -0500

    implemented whatDayIsIt

--- Task.js
+++ Task.js
@@ -5,7 +5,10 @@ import { ThemeProvider} from "react-native-rapi-ui";
 let taskText = ["I am an example task. Add more below."]
 let taskDeadline = ["02/19/23"]
 
-export function addTask(string, deadline) {
+let whatDayIsIt = new Date()
+let today = [whatDayIsIt.getMonth() + 1, whatDayIsIt.getDate(), parseInt(whatDayIsIt.getFullYear().toString().substring(2, 4))]
+
+function addTask(string, deadline) {
     if (deadline.length == 8) {
         taskText.push(string)
         taskDeadline.push(deadline)
@@ -13,13 +16,13 @@ export function addTask(string, deadline) {
     // return error if false
 }
 
-export function getTask(index) {
+function getTask(index) {
     return [taskText[index], taskDeadline[index]]
 }
 
-export function parseDate (date) {
-    let month = date.substring(0, 1)
-    let day = date.substring(3, 4)
-    let year = date.substring(6, 7)
+function parseDate (date) {
+    let month = parseInt(date.substring(0, 2))
+    let day = parseInt(date.substring(3, 5))
+    let year = parseInt(date.substring(6, 8))
     return [month, day, year]
 }
